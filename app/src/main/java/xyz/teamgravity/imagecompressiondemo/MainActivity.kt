3081bb4245352812af6573f44f85bf08d0cb6105
raheem <raheemideadev@gmail.com>
2025-01-07 23:47:52 +0500

    Installed dependencies;

--- app/src/main/java/xyz/teamgravity/imagecompressiondemo/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/imagecompressiondemo/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import xyz.teamgravity.imagecompressiondemo.ui.theme.ImageCompressionDemoTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
